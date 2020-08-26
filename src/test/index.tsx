2a8f6485cc888246ae620c4be876fc15b8904b06
RobbieGM <rgm020103@gmail.com>
2020-08-26 12:40:58 -0400

    remove accidentally committed "test" directory

--- src/test/index.tsx
+++
@@ -1,5 +0,0 @@
-import X from "react-feather/dist/icons/x";
-import { render, h } from "preact";
-
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-render(<X />, document.getElementById("main")!);
